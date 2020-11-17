4fecd7a5fc6850b757fe1dd5ea263e112459ef7d
zhaobo <xtype@qq.com>
2020-11-17 13:57:47 +0800

    Fix config.toml

--- models/models.go
+++ models/models.go
@@ -9,7 +9,7 @@ type User struct {
 	ID        int64 `gorm:"primaryKey;autoIncrement"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	Username  string `gorm:"uniqueIndex"`
+	Username  string `gorm:"uniqueIndex;size:255"`
 	Password  string
 }
 
@@ -18,7 +18,7 @@ type AdminUser struct {
 	ID          int64 `gorm:"primaryKey;autoIncrement"`
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
-	Username    string `gorm:"uniqueIndex"`
+	Username    string `gorm:"uniqueIndex;size:255"`
 	Password    string
 	LastLoginAt *time.Time
 }
