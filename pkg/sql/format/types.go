b8f1c53b926adcd13ffca20a9ae23acf6a33cb83
zhaobo <xtype@qq.com>
2020-12-30 11:16:08 +0800

    增加一个常用类型

---
+++ pkg/sql/format/types.go
@@ -0,0 +1,47 @@
+package format
+
+import "database/sql/driver"
+
+// Int64数组
+type Int64Array []int64
+
+func (t *Int64Array) Scan(src interface{}) error {
+	return Scan(t, src)
+}
+
+func (t Int64Array) Value() (driver.Value, error) {
+	return Value(t)
+}
+
+// 字符串数组
+type StringArray []string
+
+func (t *StringArray) Scan(src interface{}) error {
+	return Scan(t, src)
+}
+
+func (t StringArray) Value() (driver.Value, error) {
+	return Value(t)
+}
+
+// 字典类型
+type Map map[string]interface{}
+
+func (t *Map) Scan(src interface{}) error {
+	return Scan(t, src)
+}
+
+func (t Map) Value() (driver.Value, error) {
+	return Value(t)
+}
+
+// 字典数组
+type MapArray []Map
+
+func (t *MapArray) Scan(src interface{}) error {
+	return Scan(t, src)
+}
+
+func (t MapArray) Value() (driver.Value, error) {
+	return Value(t)
+}
