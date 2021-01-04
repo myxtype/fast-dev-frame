edd8354e39f9492cc2b4d01479d4590115ff1926
zhaobo <xtype@qq.com>
2021-01-04 13:45:13 +0800

    no message

--- pkg/i18n/i18n_test.go
+++ pkg/i18n/i18n_test.go
@@ -12,3 +12,17 @@ func TestNewI18n(t *testing.T) {
 	t.Log(i.Get("zh_CN", "title2", Options{"name": "阿狸"}))
 	t.Log(i.Get("zh_CN", "title2"))
 }
+
+func TestNewI18n2(t *testing.T) {
+	i := NewI18n(map[string]map[string]string{
+		"zh_CN": {
+			"title":  "{{name}}，你好啊！",
+			"title2": "{{name}}，{{name}}，你好吗！！",
+		},
+	})
+
+	t.Log(i.Get("zh_CN", "title", Options{"name": "阿狸"}))
+	t.Log(i.Get("zh_CN", "title", Options{"name": 33442}))
+	t.Log(i.Get("zh_CN", "title2", Options{"name": "阿狸"}))
+	t.Log(i.Get("zh_CN", "title2"))
+}
--- pkg/i18n/init.go
+++ pkg/i18n/init.go
@@ -1,30 +1,17 @@
 package i18n
 
 // 全局i18n
-var def *I18n
-
-// 创建全局默认的i18n
-func init() {
-	def = NewI18n(map[string]map[string]string{
-		"zh_CN": {
-			"请求参数错误": "请求参数错误",
-		},
-		"zh_TW": {
-			"请求参数错误": "請求參數錯誤",
-		},
-		"en_US": {
-			"请求参数错误": "Request parameter error",
-		},
-		"ja_JP": {
-			"请求参数错误": "要求パラメータエラー",
-		},
-		"ko_KR": {
-			"请求参数错误": "요청 매개 변수 오류",
-		},
-	})
-}
+var Def = NewI18n()
 
 // 通过全局的i18n获取翻译
 func Get(lang, key string, args ...Options) string {
-	return def.Get(lang, key, args...)
+	return Def.Get(lang, key, args...)
+}
+
+func SetValue(lang, key, value string) {
+	Def.SetValue(lang, key, value)
+}
+
+func SetLangValues(lang string, values map[string]string) {
+	Def.SetLangValues(lang, values)
 }
