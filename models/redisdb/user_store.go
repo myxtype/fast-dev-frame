05f0ddc874e487a6a1423dffb3f69ad63ed9617f
zhaobo <xtype@qq.com>
2021-01-04 15:01:09 +0800

    redis重命名

--- models/redisdb/user_store.go
+++ models/redisdb/user_store.go
@@ -1,4 +1,4 @@
-package rdb
+package redisdb
 
 import (
 	"context"
--- worker/bootstrap.go
+++ worker/bootstrap.go
@@ -1,7 +1,7 @@
 package worker
 
 import (
-	"frame/models/rdb"
+	"frame/models/redisdb"
 	"frame/pkg/grace"
 	"frame/pkg/queworker"
 	"frame/pkg/worker"
@@ -11,7 +11,7 @@ func StartWorker() {
 	wm := worker.NewWorkerManager()
 
 	// 用户注册成功后的消息
-	wm.AddWorker(queworker.NewQueueWorker(rdb.Shared().NewQueue("registered"), &UserRegisterHandler{}))
+	wm.AddWorker(queworker.NewQueueWorker(redisdb.Shared().NewQueue("registered"), &UserRegisterHandler{}))
 	// To add more here
 
 	grace.WorkerRun(wm)
